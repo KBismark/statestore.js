9bf4dccc8d3c425069d110d634ea4d28036520c7
KBismark <bismarkkwabenayamoah@gmail.com>
2024-06-24 07:03:29 +0000

    Export contextId type

--- lib/index.ts
+++ lib/index.ts
@@ -3,3 +3,4 @@ import * as StateStore from "./global_store";
 import * as ContextsStore from "./context_store";
 export const { getStore, subscribe, unsubscribe, updateStore, deleteProvider, deleteStore } = StateStore;
 export const {getContext, updateContext,createContext, subscribeToContext, unsubscribeToContext, destroyContext} = ContextsStore;
+export type ContextId = ContextsStore.ContextId
