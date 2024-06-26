3bd3bda875aa60f51e754a26a9bc1e55213da59c
KBismark <bismarkkwabenayamoah@gmail.com>
2024-06-26 11:14:08 +0000

    Add support for react or react-native

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,26 @@
 
 import * as StateStore from "./global_store";
 import * as ContextsStore from "./context_store";
-export const { getStore, subscribe, unsubscribe, updateStore, deleteProvider, deleteStore, createStore } = StateStore;
-export const {getContext, updateContext,createContext, subscribeToContext, unsubscribeToContext, destroyContext} = ContextsStore;
+import * as ReactStore from "./react_env"
+// Exporting at once `export const {/*...*/} = StateStore` is causing errors in final typescript output
+// Export each separately
+export const getStore = StateStore.getStore;
+export const subscribe = StateStore.subscribe;
+export const unsubscribe = StateStore.unsubscribe;
+export const updateStore = StateStore.updateStore;
+export const deleteProvider = StateStore.deleteProvider;
+export const deleteStore = StateStore.deleteStore;
+export const createStore = StateStore.createStore;
+export const getContext = ContextsStore.getContext;
+export const updateContext = ContextsStore.updateContext;
+export const createContext = ContextsStore.createContext;
+export const subscribeToContext = ContextsStore.subscribeToContext;
+export const unsubscribeToContext = ContextsStore.unsubscribeToContext;
+export const destroyContext = ContextsStore.destroyContext;
 export type ContextId = ContextsStore.ContextId
+/** ----------------------For Usage In React----------------------- */
+export const configureForReact = ReactStore.configureForReact;
+export const useStateContext = ReactStore.useStateContext;
+export const useStateStore = ReactStore.useStateStore;
+export const createComponent = ReactStore.createComponent;
+
---
+++ lib/react_env.ts
@@ -0,0 +1,39 @@
+import { ContextId, createContext, getContext, subscribeToContext, unsubscribeToContext } from "./context_store";
+import { getStore, subscribe, unsubscribe } from "./global_store";
+
+const noop = (p?:any,d?:any):any=>{}
+let R = {useState: noop, useMemo: noop, useEffect: noop, memo: noop}
+export const configureForReact = (react:any)=>{R = react};
+
+export function useStateContext<S>(contextId: ContextId,watch?: Array<keyof S>) {
+    const setState = R.useState({})[1];
+    R.useEffect(()=>{
+        if(watching){
+            const subscriptionId = subscribeToContext<S>(contextId, { watch, action:()=>setState({})});
+            return ()=>unsubscribeToContext(contextId,subscriptionId)
+        }
+    },[]);
+    const watching = !watch || watch.length > 0;
+    return getContext<S>(contextId)
+}
+
+export function useStateStore<S>(provider: string, storeId: string, watch?: Array<keyof S>) {
+    const setState = R.useState({})[1];
+    R.useEffect(()=>{
+        if(watching){
+            const subscriptionId = subscribe<S>(provider, storeId, { watch, action:()=>setState({})});
+            return ()=>unsubscribe(provider, storeId, subscriptionId)
+        }
+    },[]);
+    const watching = !watch || watch.length > 0;
+    return getStore<S>(provider, storeId)
+}
+
+
+const propsAreEqual = (a:any,b:any)=>true;
+
+export function createComponent(Component:({propsSource}:{propsSource: string|ContextId})=>any): ({propsSource}:{propsSource: string|ContextId})=>any {
+    return R.memo(Component,propsAreEqual)
+}
+
+
